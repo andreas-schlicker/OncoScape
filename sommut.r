f417e254ae535c98e706dbe4154b9699f8372add
Andreas Schlicker <a.schlicker@nki.nl>
2013-11-14 11:34:44 +0100

    Changes analyzeMuts: use all genes by default

--- sommut.r
+++ sommut.r
@@ -12,35 +12,40 @@
 # chromcol integer giving the index of the chromosome column; defaults to 5
 # startcol integer giving the index of the start column; defaults to 6
 # stopcol integer giving the index of the stop column; defaults to 7
-analyzeMuts = function(mutations, genes, ignore=c("Silent"), 
-											 samples=NULL,
+analyzeMuts = function(mutations, genes=NULL,  
+					   ignore=c("Silent"), samples=NULL,
                        genecol=1, typecol=9, samplecol=16, 
-                       chromcol=5, startcol=6, stopcol=7) {  
-  res = matrix(0, nrow=length(genes), ncol=4)
-  rownames(res) = genes
-  colnames(res) = c("total mutations", "unique mutations", "mutated samples", "unique samples")
-  
-  locMutations = mutations
-  if (!is.null(samples)) {
-  	locMutations = mutations[which(mutations[, samplecol] %in% samples), ]
-  }
-  
-  res[, 4] = length(unique(locMutations[, samplecol]))
-  
-  for (gene in genes) {
-    # Get all rows with mutations in the current gene that shouldn't be ignored
-    muts = locMutations[which(locMutations[, genecol] == gene & !(locMutations[, typecol] %in% ignore)), ]
-    
-    # Count total number of mutations
-    res[gene, "total mutations"] = nrow(muts)
-    # Count number of distinct mutations
-    # Distinct is defined as unique combination of chromosome, start and end positions
-    res[gene, "unique mutations"] = length(unique(apply(muts, 1, function(x) { paste(x[chromcol], x[startcol], x[stopcol], sep="_") })))
-    # Count the number of distinct samples with mutations in that gene
-    res[gene, "mutated samples"] = length(unique(muts[, samplecol]))
-  }
-  
-  return(res)
+                       chromcol=5, startcol=6, stopcol=7) {
+				   
+	if (is.null(genes)) { 
+		genes = unique(mutations[, genecol])
+	}
+	
+	res = matrix(0, nrow=length(genes), ncol=4)
+	rownames(res) = genes
+	colnames(res) = c("total mutations", "unique mutations", "mutated samples", "unique samples")
+	
+	locMutations = mutations
+	if (!is.null(samples)) {
+		locMutations = mutations[which(mutations[, samplecol] %in% samples), ]
+	}
+	
+	res[, 4] = length(unique(locMutations[, samplecol]))
+	
+	for (gene in genes) {
+		# Get all rows with mutations in the current gene that shouldn't be ignored
+		muts = locMutations[which(locMutations[, genecol] == gene & !(locMutations[, typecol] %in% ignore)), ]
+		
+		# Count total number of mutations
+		res[gene, "total mutations"] = nrow(muts)
+		# Count number of distinct mutations
+		# Distinct is defined as unique combination of chromosome, start and end positions
+		res[gene, "unique mutations"] = length(unique(apply(muts, 1, function(x) { paste(x[chromcol], x[startcol], x[stopcol], sep="_") })))
+		# Count the number of distinct samples with mutations in that gene
+		res[gene, "mutated samples"] = length(unique(muts[, samplecol]))
+	}
+	
+	return(res)
 }
 
 # Summarizes mutation scores. 
