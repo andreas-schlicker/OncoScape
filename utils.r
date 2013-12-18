626a7ce9e82804cd9901162baa9a84e1efc6a3f5
Andreas Schlicker <a.schlicker@nki.nl>
2013-12-18 11:39:22 +0100

    Adds new option for the methylation command line option

--- utils.r
+++ utils.r
@@ -362,7 +362,7 @@ getOptionList = function(options) {
 			   prefix=make_option(c("-p", "--prefix"), type="character", help="Plotting file prefix", default=""),
 			   notopgenes=make_option(c("-n", "--notopgenes"), type="integer", help="Number of top genes", default=10),
 			   datatype=make_option(c("-d", "--datatype"), type="character", help="Comma-separated list of data types to test (all, exprs, achilles, cgh, meth, sommut)", default="all"),
-			   methylation=make_option(c("-m", "--methylation"), type="character", help="Resolution of methylation data (base or region)", default="base"),
+			   methylation=make_option(c("-m", "--methylation"), type="character", help="Resolution of methylation data (base, region or region_filtered)", default="base"),
 			   filtercol=make_option(c("-f", "--filtercol"), type="character", help="Name of the column with the functional mutation score", default="NULL"),
 			   filterscore=make_option(c("-r", "--filterscore"), type="numeric", help="Minimum functional score a mutation needs to have", default="1"),
 			   filtersnps=make_option(c("-l", "--filtersnps"), type="character", help="Determines whether methylation probes containing SNPs should be filtered out (comma-separated list of SNP_target, SNP_within_10, SNP_outside_10, SNP_probe or none)", default="SNP_target,SNP_within_10,SNP_outside_10,SNP_probe"),
