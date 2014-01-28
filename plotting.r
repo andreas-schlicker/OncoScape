09ddb5f9b4da1abfaeecadde65fa7aa6e6d43d5f
Andreas Schlicker <a.schlicker@nki.nl>
2014-01-28 16:59:14 +0100

    Fixes bug in conHeatDf: don't expect scores to be either 0 or 1

--- plotting.r
+++ plotting.r
@@ -620,8 +620,8 @@ conHeatDf = function(dataframe) {
 	res.df = data.frame()
 	for (cancType in unique(dataframe[, "cancer"])) {
 		tempTab = table(subset(dataframe, cancer == cancType)[, c("score", "score2")])
-		res.df = rbind(res.df, data.frame(x=rep(rownames(tempTab), times=2),
-										  y=rep(colnames(tempTab), each=2),
+		res.df = rbind(res.df, data.frame(x=rep(rownames(tempTab), times=nrow(tempTab)),
+										  y=rep(colnames(tempTab), each=ncol(tempTab)),
 										  freq=as.vector(tempTab),
 										  cancer=cancType))
 	}
